a33930064bdb98091422522d4e75c00e60e80d98
Rulu <hafnium1923@gmail.com>
2024-04-02 12:15:47 +0900

    feat: font 굵기에 따른 종류 추가

--- src/components/common/Title/Title.styles.ts
+++ src/components/common/Title/Title.styles.ts
@@ -9,8 +9,10 @@ export const titleContainer = css`
 
 export const explainText = css`
   font-size: 50px;
+  font-weight: 800;
 `;
 
 export const titleText = css`
   font-size: 180px;
+  font-weight: 800;
 `;
