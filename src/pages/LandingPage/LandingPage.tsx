2c1cd314f0c4020840c5370516dad11fb6fd0e4d
Rulu <hafnium1923@gmail.com>
2024-04-03 14:28:40 +0900

    refactor: 랜딩 페이지 스타일 파일 분리

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,32 +1,11 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import * as S from '~/pages/LandingPage/LandingPage.styles';
 import Title from '~/components/common/Title/Title';
 import LogInModal from '~/components/landing/LogInModal/LogInModal';
 
-const landingPageContainer = css`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-
-  width: 100vw;
-  height: 90vh;
-  padding: 20px 40px;
-`;
-
-const titleAndLogInButtonContainer = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  height: 100%;
-  width: fit-content;
-  gap: 10px;
-`;
-
 const LandingPage = () => {
   return (
-    <div css={landingPageContainer}>
-      <div css={titleAndLogInButtonContainer}>
+    <div css={S.landingPageContainer}>
+      <div css={S.titleAndLogInButtonContainer}>
         <Title />
         <LogInModal />
       </div>
