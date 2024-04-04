142db01f371cc28b3aee7a7c6f463e88818efee1
Rulu <hafnium1923@gmail.com>
2024-04-04 21:43:53 +0900

    feat: 프로젝트 정보에 대한 전역상태 구현

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,12 +2,14 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { ROUTES } from '~/constants/routes';
 import { LOCAL_STORAGE } from '~/constants/api';
+import { useGetUserProjectInfo } from '~/hooks/@query/useGetUserProjectInfo';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [params] = useSearchParams();
   const accessToken = params.get('accessToken');
   const refreshToken = params.get('refreshToken');
+  const { projectInfo, isFetched } = useGetUserProjectInfo();
 
   useEffect(() => {
     if (!accessToken || !refreshToken) {
@@ -18,17 +20,20 @@ const LoginPage = () => {
 
       return;
     }
-  }, []);
 
-  useEffect(() => {
     if (accessToken && refreshToken) {
       localStorage.setItem(LOCAL_STORAGE.ACCESS_TOKEN, accessToken);
       localStorage.setItem(LOCAL_STORAGE.REFRESH_TOKEN, refreshToken);
-
-      navigate(ROUTES.START);
     }
   }, []);
 
+  useEffect(() => {
+    if (isFetched) {
+      projectInfo.projectId === 0
+        ? navigate(ROUTES.START)
+        : navigate(ROUTES.DASH_BOARD);
+    }
+  }, [isFetched]);
   return <></>;
 };
 
