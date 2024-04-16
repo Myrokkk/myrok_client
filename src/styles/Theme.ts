68fde7be71bce01eff2a4202ea98ea3bb50b1622
Rulu <hafnium1923@gmail.com>
2024-04-16 22:12:29 +0900

    refactor: gray900 색상 black으로 변경

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,7 +3,7 @@ const color = {
   primary500: '#A356F0',
   white: '#fff',
   red: '#ff5b5b',
-  gray900: '#2b2b2b;',
+  black: '#2b2b2b',
   gray800: '#5c5b5b',
   gray300: '#c4c0c0',
   gray200: '#dbd9d9',
