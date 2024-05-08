12a35a698aa5ceb868863165890db062031dba8f
Rulu <hafnium1923@gmail.com>
2024-05-08 17:28:35 +0900

    feat: 참가자 Box 로직 구현

--- src/components/meeting_minutes/ParticipantInfoBox/ParticipantInfoBox.tsx
+++ src/components/meeting_minutes/ParticipantInfoBox/ParticipantInfoBox.tsx
@@ -1,20 +1,43 @@
+import { useState } from 'react';
 import Tag from '~/components/common/Tag/Tag';
 import Text from '~/components/common/Text/Text';
+import { useGetProjectMemberNames } from '~/hooks/@query/useGetProjectMemberNames';
+import * as S from '~/components/meeting_minutes/ParticipantInfoBox/ParticipantInfoBox.styles';
+
+interface ParticipantInfoBoxProps {
+  handleMemberListChange: (memberList: number[]) => void;
+}
+
+const ParticipantInfoBox = (props: ParticipantInfoBoxProps) => {
+  const { projectMemberNames } = useGetProjectMemberNames();
+  const [participants, setParticipants] = useState<number[]>([]);
+  const { handleMemberListChange } = props;
+
+  const handleParticipantsCheck = (memberId: number) => {
+    if (participants.includes(memberId)) {
+      handleMemberListChange([...participants.filter((id) => id !== memberId)]);
+      setParticipants((prev) => prev.filter((id) => id !== memberId));
+    } else {
+      handleMemberListChange([...participants, memberId]);
+      setParticipants((prev) => [...prev, memberId]);
+    }
+  };
 
   return (
-    <div css={participantInfoBoxContainer}>
+    <div css={S.participantInfoBoxContainer}>
       <Text size="xxl" weight="bold">
         참가자
       </Text>
-      <div css={participantContainer}>
-        {members.map((member) => {
+      <div css={S.participantContainer}>
+        {projectMemberNames.map((projectMember) => {
+          const { memberId, name } = projectMember;
           return (
             <Tag.Check
-              key={member}
-              css={tagStyling}
+              key={memberId}
+              css={S.tagStyling}
               act="check"
-              handleTagCheck={() => {}}
-              content={member}
+              handleTagCheck={() => handleParticipantsCheck(memberId)}
+              content={name}
             />
           );
         })}
