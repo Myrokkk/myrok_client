22ae32c8a12082a5f3f4c5a6fa9fd0c0427645aa
Rulu <hafnium1923@gmail.com>
2024-05-08 23:48:45 +0900

    refactor: 회의록 조회 페이지 스타일 분리

--- src/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.tsx
+++ src/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react';
 import { useParams } from 'react-router-dom';
 import Tag from '~/components/common/Tag/Tag';
 import Text from '~/components/common/Text/Text';
@@ -6,105 +5,7 @@ import { useGetRecord } from '~/hooks/@query/useGetRecord';
 import NotFoundPage from '~/pages/NotFoundPage/NotFoundPage';
 import { Theme } from '~/styles/Theme';
 import { generateDateToStringFormat } from '~/utils/generateDateToStringFormat';
-
-const meetingMinutesRecordPageContainer = css`
-  display: flex;
-  justify-content: space-between;
-  padding: 30px;
-  height: calc(100% - 160px);
-`;
-
-const recordDetailContainer = css`
-  width: 60%;
-  min-height: 400px;
-  max-height: 700px;
-  overflow-y: auto;
-  padding: 10px;
-
-  background-color: ${Theme.color.white};
-  border-radius: 12px;
-  border: 1px solid ${Theme.color.gray200};
-`;
-
-const recordWriteInfoBoxContainer = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  min-width: 300px;
-  max-width: 460px;
-  height: fit-content;
-  gap: 20px;
-  padding: 30px;
-
-  background-color: ${Theme.color.white};
-  border-radius: 20px;
-  box-shadow: 0 10px 20px ${Theme.color.gray250};
-`;
-
-const titleContainer = css`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 10px;
-`;
-
-const tagContainer = css`
-  display: flex;
-  justify-content: space-between;
-  max-height: 130px;
-  gap: 2px;
-`;
-
-const tagsGrid = css`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-end;
-  gap: 6px;
-
-  max-width: 362px;
-  max-height: 84px;
-  overflow-y: auto;
-`;
-
-const participantInfoBoxContainer = css`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  max-width: 460px;
-  max-height: 160px;
-  padding: 30px;
-
-  background-color: ${Theme.color.white};
-  border-radius: 20px;
-  box-shadow: 0 10px 20px ${Theme.color.gray250};
-`;
-
-const participantContainer = css`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-
-  max-width: 220px;
-  max-height: 100px;
-  overflow-y: auto;
-`;
-
-const tagStyling = css`
-  padding: 10px 20px;
-
-  font-size: 20px;
-  font-weight: 800;
-`;
-
-const infoContainer = css`
-  display: flex;
-  flex-direction: column;
-  gap: 60px;
-
-  height: 100%;
-`;
+import * as S from '~/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.styles';
 
 const MeetingMinutesRecordPage = () => {
   const recordId = useParams().recordId;
@@ -125,13 +26,13 @@ const MeetingMinutesRecordPage = () => {
   )?.name;
 
   return (
-    <div css={meetingMinutesRecordPageContainer}>
-      <div css={recordDetailContainer}>
+    <div css={S.meetingMinutesRecordPageContainer}>
+      <div css={S.recordDetailContainer}>
         <div dangerouslySetInnerHTML={{ __html: recordContent }}></div>
       </div>
-      <div css={infoContainer}>
-        <div css={recordWriteInfoBoxContainer}>
-          <div css={titleContainer}>
+      <div css={S.infoContainer}>
+        <div css={S.recordWriteInfoBoxContainer}>
+          <div css={S.titleContainer}>
             <Text css={{ width: '70px' }} size="xxl" weight="bold">
               회의명
             </Text>
@@ -139,7 +40,7 @@ const MeetingMinutesRecordPage = () => {
               {recordName}
             </Text>
           </div>
-          <div css={titleContainer}>
+          <div css={S.titleContainer}>
             <Text css={{ width: '70px' }} size="xxl" weight="bold">
               작성자
             </Text>
@@ -147,17 +48,17 @@ const MeetingMinutesRecordPage = () => {
               {recordWriterName === undefined ? '' : recordWriterName}
             </Text>
           </div>
-          <div css={titleContainer}>
+          <div css={S.titleContainer}>
             <Text css={{ width: '70px' }} size="xxl" weight="bold">
               작성일
             </Text>
             <Text weight="bold">{generateDateToStringFormat(recordDate)}</Text>
           </div>
-          <div css={tagContainer}>
+          <div css={S.tagContainer}>
             <Text css={{ width: '70px' }} size="xxl" weight="bold">
               태그
             </Text>
-            <div css={tagsGrid}>
+            <div css={S.tagsGrid}>
               {tagList.map((newTag, i) => {
                 const color = Theme.presetColors[i % 10];
                 return (
@@ -172,11 +73,11 @@ const MeetingMinutesRecordPage = () => {
             </div>
           </div>
         </div>
-        <div css={participantInfoBoxContainer}>
+        <div css={S.participantInfoBoxContainer}>
           <Text size="xxl" weight="bold">
             참가자
           </Text>
-          <div css={participantContainer}>
+          <div css={S.participantContainer}>
             {memberList.map((member) => {
               const { memberId, name } = member;
               return (
@@ -187,7 +88,7 @@ const MeetingMinutesRecordPage = () => {
                       ? Theme.color.primary500
                       : Theme.color.gray800
                   }
-                  css={tagStyling}
+                  css={S.tagStyling}
                   content={name}
                 />
               );
