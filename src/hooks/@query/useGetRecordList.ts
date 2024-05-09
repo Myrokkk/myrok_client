2e98c9cb4644415fbf5d0d75201f77047c19a77c
Rulu <hafnium1923@gmail.com>
2024-05-09 19:33:49 +0900

    refactor: 회의록 목록 조회 url 변경

--- src/hooks/@query/useGetRecordList.ts
+++ src/hooks/@query/useGetRecordList.ts
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getRecordList } from '~/apis/record';
+import { useProject } from '~/hooks/useProject';
 import type { RecordInfo } from '~/types/record';
 
 export const useGetRecordList = () => {
+  const { projectId } = useProject();
+
   const { data, isFetched } = useQuery({
-    queryKey: ['recordList'],
-    queryFn: getRecordList,
+    queryKey: ['recordList', projectId],
+    queryFn: () => getRecordList(projectId),
   });
 
   if (data === undefined) return { recordInfoList: [] as RecordInfo[] };
