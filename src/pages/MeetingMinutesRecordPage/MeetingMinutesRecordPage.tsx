35cc8746677a18fc7dd0a24b3f16de5748a2d387
Rulu <hafnium1923@gmail.com>
2024-05-16 17:22:11 +0900

    feat: 작성자에게만 수정& 삭제 버튼 노출

--- src/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.tsx
+++ src/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.tsx
@@ -7,6 +7,8 @@ import { Theme } from '~/styles/Theme';
 import { generateDateToStringFormat } from '~/utils/generateDateToStringFormat';
 import * as S from '~/pages/MeetingMinutesRecordPage/MeetingMinutesRecordPage.styles';
 import SummaryBox from '~/components/meeting_minutes/SummaryBox/SummaryBox';
+import Button from '~/components/common/Button/Button';
+import { useGetUserInfo } from '~/hooks/@query/useGetUserInfo';
 
 const MeetingMinutesRecordPage = () => {
   const recordId = useParams().recordId;
@@ -22,6 +24,7 @@ const MeetingMinutesRecordPage = () => {
     memberList,
     recordDate,
   } = useGetRecord(Number(recordId));
+  const { memberId } = useGetUserInfo();
 
   const recordWriterName = memberList.find(
     (member) => member.memberId === recordWriterId,
@@ -98,6 +101,14 @@ const MeetingMinutesRecordPage = () => {
             })}
           </div>
         </div>
+        {recordWriterId === memberId && (
+          <div css={S.writerButton}>
+            <Button css={S.buttonStyling} variant="primary">
+              수정하기
+            </Button>
+            <Button css={S.deleteButtonStyling}>삭제하기</Button>
+          </div>
+        )}
       </div>
     </div>
   );
