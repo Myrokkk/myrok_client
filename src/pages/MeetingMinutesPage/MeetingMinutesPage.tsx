2667e84c0d6344f3332d43553c690c41adb30511
Rulu <hafnium1923@gmail.com>
2024-05-30 23:02:42 +0900

    feat: 회의록 목록페이지의 태그목록으로 url 이동

--- src/pages/MeetingMinutesPage/MeetingMinutesPage.tsx
+++ src/pages/MeetingMinutesPage/MeetingMinutesPage.tsx
@@ -4,17 +4,33 @@ import { useEffect, useState } from 'react';
 import type { RecordInfo } from '~/types/record';
 import { useGetRecordList } from '~/hooks/@query/useGetRecordList';
 import RecordTableHeader from '~/components/meeting_minutes/RecordTableHeader/RecordTableHeader';
+import TagListMenu from '~/components/meeting_minutes/TagListMenu/TagListMenu';
+import { useGetRecordTagList } from '~/hooks/@query/useGetRecordTagList';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { ROUTES } from '~/constants/routes';
 
 const meetingMinutesPageContainer = css`
   padding: 30px;
 `;
 
+const meetingMinutesMainContainer = css`
+  display: flex;
+  gap: 30px;
+`;
+
 const MeetingMinutesPage = () => {
+  const [params] = useSearchParams();
+  const tagParam = params.get('tagName');
   const [recordList, setRecordList] = useState<RecordInfo[]>([]);
   const [totalPage, setTotalPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const [sortMethod, setSortMethod] = useState<'new' | 'old'>('new');
+  const [clickedTag, setClickedTag] = useState(
+    tagParam === null ? '전체보기' : tagParam,
+  );
   const { recordInfoList, isFetched } = useGetRecordList();
+  const { tagList, totalCount } = useGetRecordTagList();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isFetched) {
@@ -37,6 +53,16 @@ const MeetingMinutesPage = () => {
     setRecordList([...recordInfoList].reverse());
   };
 
+  const handleTagClick = (tag: string) => {
+    setClickedTag(tag);
+    const url =
+      tag === '전체보기'
+        ? ROUTES.MEETING_MINUTES
+        : `${ROUTES.MEETING_MINUTES}?tagName=${tag}`;
+
+    navigate(url);
+  };
+
   return (
     <div css={meetingMinutesPageContainer}>
       <RecordTableHeader
@@ -44,12 +70,22 @@ const MeetingMinutesPage = () => {
         handleListNewSort={handleListNewSort}
         handleListOldSort={handleListOldSort}
       />
-      <RecordTable
-        recordList={recordList}
-        currentPage={currentPage}
-        totalPage={totalPage}
-        handlePageChange={handlePageChange}
-      />
+      <div css={meetingMinutesMainContainer}>
+        {totalCount !== 0 && (
+          <TagListMenu
+            totalCount={totalCount}
+            tagList={tagList}
+            clickedTag={clickedTag}
+            handleTagClick={handleTagClick}
+          />
+        )}
+        <RecordTable
+          recordList={recordList}
+          currentPage={currentPage}
+          totalPage={totalPage}
+          handlePageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
