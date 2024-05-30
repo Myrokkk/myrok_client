35e56f448608eb6899568f5a0498f0b18d4e7a93
Rulu <hafnium1923@gmail.com>
2024-05-30 21:53:44 +0900

    refactor: 회의록페이지 최상단에서 회의록 목록 데이터 관리

---
+++ src/components/meeting_minutes/TagListMenu/TagListMenu.tsx
@@ -0,0 +1,5 @@
+const TagListMenu = () => {
+  return <div> 태그 목록</div>;
+};
+
+export default TagListMenu;
--- src/pages/MeetingMinutesPage/MeetingMinutesPage.tsx
+++ src/pages/MeetingMinutesPage/MeetingMinutesPage.tsx
@@ -1,14 +1,57 @@
 import RecordTable from '~/components/meeting_minutes/RecordTable/RecordTable';
 import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import type { RecordInfo } from '~/types/record';
+import { useNavigate } from 'react-router-dom';
+import { useGetRecordList } from '~/hooks/@query/useGetRecordList';
+import RecordTableHeader from '~/components/meeting_minutes/RecordTableHeader/RecordTableHeader';
 
 const meetingMinutesPageContainer = css`
   padding: 30px;
 `;
 
 const MeetingMinutesPage = () => {
+  const [recordList, setRecordList] = useState<RecordInfo[]>([]);
+  const [totalPage, setTotalPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortMethod, setSortMethod] = useState<'new' | 'old'>('new');
+  const navigate = useNavigate();
+  const { recordInfoList, isFetched } = useGetRecordList();
+
+  useEffect(() => {
+    if (isFetched) {
+      setRecordList(recordInfoList);
+      setTotalPage(Math.ceil(recordInfoList.length / 8));
+      setCurrentPage(1);
+    }
+  }, [recordInfoList, isFetched]);
+
+  const handlePageChange = (pageNum: number) => {
+    setCurrentPage(pageNum);
+  };
+
+  const handleListNewSort = () => {
+    setSortMethod('new');
+    setRecordList(recordInfoList);
+  };
+  const handleListOldSort = () => {
+    setSortMethod('old');
+    setRecordList([...recordInfoList].reverse());
+  };
+
   return (
     <div css={meetingMinutesPageContainer}>
-      <RecordTable />
+      <RecordTableHeader
+        sortMethod={sortMethod}
+        handleListNewSort={handleListNewSort}
+        handleListOldSort={handleListOldSort}
+      />
+      <RecordTable
+        recordList={recordList}
+        currentPage={currentPage}
+        totalPage={totalPage}
+        handlePageChange={handlePageChange}
+      />
     </div>
   );
 };
