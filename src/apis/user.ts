1533bfd64d26ef217f8301669b07375fc7753f0b
Rulu <hafnium1923@gmail.com>
2024-10-13 19:34:33 +0900

    Update user.ts

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -4,6 +4,7 @@ import { LOCAL_STORAGE } from '~/constants/api';
 import type { UserInfo, UserProjectInfo } from '~/types/user';
 
 export const getGoogleLogin = async () => {
+  console.log(process.env.REACT_APP_BASE_URL);
   const { data } = await instance.get<{ loginUrl: string }>(
     '/myrok/auth/google',
   );
